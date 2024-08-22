d6f301b92d107d024ce6fbe844d997f822ca4c09
Nando <desiderato@exactls.com>
2024-08-22 16:49:02 +0200

    nascosto manage videos per user

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,8 @@
             </svg>
             Favorites
           </router-link>
-          <router-link to="/video-management" class="flex items-center py-3 px-6 text-lg text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700">
+          <!-- Mostra il link "Manage Videos" solo per gli utenti admin -->
+          <router-link v-if="isAdminUser" to="/video-management" class="flex items-center py-3 px-6 text-lg text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3" viewBox="0 0 20 20" fill="currentColor">
               <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
             </svg>
