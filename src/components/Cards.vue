9eeb1f85ce2f56606b5956ba2fe2478d9433912b
FraLab <francescalabellarte@gmail.com>
2024-09-11 12:55:57 +0200

    Ripristinati favourites

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -42,11 +42,31 @@
         </div>
       </div>
       <div class="p-2">
-        <h3
-          class="text-sm font-semibold mb-1 text-gray-900 dark:text-white line-clamp-2"
-        >
-          {{ video.title }}
-        </h3>
+        <div class="top-wrapper">
+          <h3
+            class="text-sm font-semibold mb-1 text-gray-900 dark:text-white line-clamp-2"
+          >
+            {{ video.title }}
+          </h3>
+          <button
+            @click.stop="toggleFavorite(video)"
+            class="text-yellow-500 hover:text-yellow-600"
+          >
+            <svg
+              data-v-7a7a37b1=""
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              viewBox="0 0 20 20"
+              :fill="isFavorite(video) ? '#ffc356' : 'none'"
+            >
+              <path
+                data-v-7a7a37b1=""
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+
         <p class="text-xs text-gray-600 dark:text-gray-300 mb-1">
           {{ formatDuration(video.duration) }}
         </p>
@@ -93,7 +113,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import "../assets/cards.scss";
 
 interface Video {
@@ -121,11 +141,29 @@ export default defineComponent({
   },
   emits: ["open-video-modal"],
   setup(props) {
+    const favorites = ref<string[]>(
+      JSON.parse(localStorage.getItem("favorites") || "[]")
+    );
+
     const getFullUrl = (url: string) => {
       if (url.startsWith("http")) {
         return url;
       }
       return `${props.baseUrl}${url}`;
+    };
+
+    const isFavorite = (video: Video) => {
+      return favorites.value.includes(video.id);
+    };
+
+    const toggleFavorite = (video: Video) => {
+      const index = favorites.value.indexOf(video.id);
+      if (index === -1) {
+        favorites.value.push(video.id);
+      } else {
+        favorites.value.splice(index, 1);
+      }
+      localStorage.setItem("favorites", JSON.stringify(favorites.value));
     };
 
     const formatDuration = (seconds: number) => {
@@ -152,6 +190,8 @@ export default defineComponent({
       formatDuration,
       isCompleted,
       getCompletionPercentage,
+      toggleFavorite,
+      isFavorite,
     };
   },
 });
